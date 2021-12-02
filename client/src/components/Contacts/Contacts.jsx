a149d27f1351d3db8c2fdd4069352cf1e9ca0627
Yanislav Petrov <valsinay@yahoo.bg>
2021-12-03 01:39:22 +0200

    Contact form  css updated

--- client/src/components/Contacts/Contacts.jsx
+++ client/src/components/Contacts/Contacts.jsx
@@ -1,45 +1,48 @@
-import React, { Component } from 'react';
-import styles from './Contacts.module.scss'
+import React, { Component } from "react";
+import styles from "./Contacts.module.scss";
 
-class Contacts extends Component {
+export default function Contacts() {
+  return (
+    <form className={styles.form}>
+      <h2 className={styles.contactHeading}>Contact form</h2>
+      <input
+        type="name"
+        name="name"
+        autoComplete="off"
+        placeholder="Your Name"
+        className={styles.username}
+        // value={data.username}
+        //onChange={this.handleChange}
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Your Email"
+        autoComplete="off"
+        className={styles.email}
+        // value={data.password}
+        // onChange={this.handleChange}
+      />
+      <input
+        type="phone"
+        name="phone"
+        placeholder="Phone"
+        autoComplete="off"
+        className={styles.phone}
+      />
 
-    render() {
-        return (
-            <form className='authForm contact' >
-                <h2>Contact form</h2>
-                <span>Send us a message</span>
-                <input
-                    type='name'
-                    name="name"
-                    placeholder="Your Name"
-                // value={data.username}
-                //onChange={this.handleChange}
-                />
-                <input
-                    type='email'
-                    name="email"
-                    placeholder="Your Email"
-                // value={data.password}
-                // onChange={this.handleChange}
-                />
-                <input
-                    type='phone'
-                    name='phone'
-                    placeholder="Phone"
-                />
-                <textarea
-                    type='text'
-                    name="text"
-                    placeholder="Your message"
-                // value={data.confirmPassword}
-                // onChange={this.handleChange}
-                />
-                <button className="submitBtn" type='submit'>Submit</button>
-            </form>
-        )
-    }
+      <textarea
+        type="text"
+        name="text"
+        autoComplete="off"
+        placeholder="Your message"
+        className={styles.message}
+        // value={data.confirmPassword}
+        // onChange={this.handleChange}
+      />
+      <button className={styles.submitBtn} type="submit">
+        Submit
+      </button>
+    </form>
+  );
 }
-
-export default Contacts;
-
-
